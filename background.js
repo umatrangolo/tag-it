8daa56972e4c892db2f461fec967c1c458bbf54e
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-01-10 11:48:42 +0000

    Saving current url in the localStorage

--- background.js
+++ background.js
@@ -1,5 +1,32 @@
-console.log("Binding...");
+var Store = {
+
+    save: function(url) {
+        if (localStorage.tagit == undefined) {
+            localStorage.tagit = JSON.stringify({
+                "journal": []
+            });
+        }
+
+        var tagit = JSON.parse(localStorage.tagit);
+        tagit.journal.unshift(url);
+
+        localStorage.tagit = JSON.stringify(tagit);
+
+        console.log("Tagged " + url + " has been saved in the Local Storage");
+    },
+
+    loadAll: function() {
+        console.log("Loading the journal ...");
+    }
+};
+
 chrome.commands.onCommand.addListener(function(command) {
-    console.log('onCommand event received for message: ', command);
-    chrome.tabs.create({url: "news.ycombinator.com"});
+    if (command == "save") {
+        // TODO we assume that the current tab is the first one from the query
+        // TODO extract the minimal url (e.g: https://www.mywickr.com instead of https://www.mywickr.com/en/index.php)
+        chrome.tabs.query({ "active": true }, function(tab) {
+            Store.save(tab[0].url);
+        });
+    }
+    else Store.loadAll();
 });
