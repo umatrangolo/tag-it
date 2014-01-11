52a51e8dda658e17eb46cde9c42bac14842f37e4
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-01-11 23:02:31 +0000

    Reorganizing the code a little bit

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+var JournalGenerator = {
+
+    showJournal: function(journal) {
+        var journalUl = document.getElementById('journal-list');
+
+        journal.forEach(function(j) {
+            var li = document.createElement('li');
+
+            li.innerHTML = '<a href="' + j + '">' + j + '</a>';
+            journalUl.appendChild(li);
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   var journal = Store.loadAll();
   console.log("Journal is " + journal);
