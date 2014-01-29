423769b8c571257e5f818d89e850c4fbd158820a
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-01-29 23:44:09 +0000

    Moved to chrome.storage.sync to store data

--- js/store.js
+++ js/store.js
@@ -1,41 +1,53 @@
 var Store = {
-    save: function(title, url) {
-        if (localStorage.tagit == undefined) {
-            localStorage.tagit = JSON.stringify({
-                "journal": []
-            });
+    save: function(title, url, continuation) {
+        chrome.storage.sync.get("tagit", function(tagit) {
+            if (tagit == {}) {
+                var journal = append(title, url, []);
+
+                chrome.storage.sync.set({
+                    "tagit" : { "journal": journal }
+                }, function() {
+                    console.log("TagIt storage area has been initialized.");
+                    chrome.storage.sync.get("tagit", continuation);
+                });
+            } else {
+                var journal = append(title, url, tagit.journal)
+
+                chrome.storage.sync.set({ "tagit" : { "journal" : journal } }, function() {
+                    console.log("TagIt Journal has been updated for url " + url);
+                    chrome.storage.sync.get("tagit", continuation);
+                });
+            }
+        });
+
+        function append(title, url, journal) {
+            var item = {
+                "id": Date.now(),
+                "url": url,
+                "title": title,
+                "deleted": false
+            };
+
+            return journal.unshift(item);
         }
-
-        var tagit = JSON.parse(localStorage.tagit);
-        var item = {
-            "id": Date.now(),
-            "url": url,
-            "title": title,
-            "deleted": false
-        };
-
-        tagit.journal.unshift(item);
-        localStorage.tagit = JSON.stringify(tagit);
     },
 
     delete: function(id) {
-        if (localStorage.tagit != undefined) {
-            var tagit = JSON.parse(localStorage.tagit);
-
-            tagit.journal.forEach(
-                function(item) {
-                    if (item.id == id) {
-                        item.deleted = true;
-                    }
+        chrome.storage.sync.get("tagit", function(tagit) {
+            _.each(tagit.journal, function(e) {
+                if (e.id == id) {
+                    e.deleted = true;
                 }
-            );
-            localStorage.tagit = JSON.stringify(tagit);
-        }
+            });
+
+            chrome.storage.sync.set({"tagit": tagit}, function() {
+                console.log("Journal item with id: " + id + " has been deleted");
+                chrome.storage.sync.get("tagit", continuation);
+            });
+        });
     },
 
-    loadAll: function() {
-        var tagit = JSON.parse(localStorage.tagit);
-        if (tagit) return tagit.journal;
-        else [];
+    loadAll: function(continuation) {
+        chrome.storage.sync.get("tagit", function(tagit) { continuation(tagit); });
     }
 };
