aa0871815c3a6c705fd616259ea2caf5823aad6e
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-04-16 23:23:03 +0100

    Adding `Import` tab

--- js/export.js
+++ js/export.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    Store.open(function(db) {
-        Store.loadAll(db, function(journal) {
-            var journalNotDeleted = _.filter(journal, function(e) {
-                return e.deleted != true;
-            });
-
-            var jsonJournal = JSON.stringify(journalNotDeleted, undefined, 2);
-            document.getElementById('journal-export').innerText = jsonJournal;
+  Store.open(function(db) {
+    Store.loadAll(db, function(journal) {
+      var journalNotDeleted = _.filter(journal,
+        function(e) {
+          return e.deleted != true;
         });
+
+      var jsonJournal = JSON.stringify(journalNotDeleted, undefined, 2);
+      document.getElementById('journal-export').innerText = jsonJournal;
     });
+  });
 });
