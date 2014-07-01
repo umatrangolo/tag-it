52c80055698413a22d26795ecf5c6f9e19b241f2
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-07-01 13:53:56 +0200

    (fix) Using Google to fetch favicons

--- js/utils.js
+++ js/utils.js
@@ -2,5 +2,9 @@ var Utils = {
 
   stripUrl: function(url) {
     return new URL(url).hostname;
+  },
+
+  urlForFavicon: function(domain) {
+    return "http://www.google.com/s2/favicons?domain=" + domain;
   }
 };
