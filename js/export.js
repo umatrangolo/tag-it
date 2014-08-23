e667f6806675fd37138b4a1579b0b49d5b1e20d6
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-08-23 11:08:43 +0100

    Better export logic filtering pointless data

--- js/export.js
+++ js/export.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   Store.open(function(db) {
     Store.loadAll(db, function(journal) {
-      var journalNotDeleted = _.filter(journal, function(e) {
-        return e.deleted != true;
-      });
 
-      var jsonJournal = JSON.stringify(journalNotDeleted, undefined, 2);
+      function filterDeleted(journal) {
+        return _.filter(journal, function(e) { return e.deleted != true; });
+      }
+
+      function removeFavicon(journal) {
+        return _.map(journal, function(e) { return _.omit(e, 'id', 'favicon', 'deleted'); });
+      }
+
+      var exportableJournal = _.compose(filterDeleted, removeFavicon)(journal);
+      var jsonJournal = JSON.stringify(exportableJournal, undefined, 2);
       document.getElementById('journal-export').innerText = jsonJournal;
     });
   });
